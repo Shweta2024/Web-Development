9c1b796df8c93fa1c1716cf28ac352c37047b499
Shweta Bhagat <bhagatshweta0216@gmail.com>
2022-11-04 22:38:20 +0530

    add constructor function

---
+++ JAVASCRIPT/Constructor-Function/constructor-fuction.js
@@ -0,0 +1,10 @@
+function Student(name, age, roll, languages) {
+    this.name = name;
+    this.age = age;
+    this.roll = roll;
+    this.languages = languages;
+}
+
+var shweta = new Student("shweta", 20, 90, ["C", "C++", "JavaScript", "Python"]);
+var sahil = new Student("sahil", 18, 2, ["C++", "python"]);
+
