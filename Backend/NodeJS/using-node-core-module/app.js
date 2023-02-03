76534270786f39f55205ea31a045993746d0354d
Shweta Bhagat <75883328+Shweta2024@users.noreply.github.com>
2023-02-03 19:35:50 +0530

    Delete Backend/NodeJS/using-node-core-module directory

--- Backend/NodeJS/using-node-core-module/app.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-//creates file & add data to it if file doesn't priorly exists
-//if file exists,then data gets overwritten
-fs.writeFileSync('text.txt', 'I am shweta.');
-
-
-//appends data in the specified file
-fs.appendFileSync('text.txt', 'I am a backend developer.');
